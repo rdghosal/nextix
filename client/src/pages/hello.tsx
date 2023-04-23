c28ea682b9f7396c114535022af363654fffc019
Rahul Ghosal <rdghosal@gmail.com>
2023-04-22 20:45:17 -0400

    chore: add log when hitting next

--- client/src/pages/hello.tsx
+++ client/src/pages/hello.tsx
@@ -4,6 +4,7 @@ export default function Landing() {
     const [ myData, setMyData ] = useState<string>();
     useEffect(() => {
         if (!myData) {
+            console.log('hitting server');
             fetch('/api/hello')
                 .then(res => res.json())
                 .then(data => setMyData(data.name))
